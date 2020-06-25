91415b4bdf44fcbcf5253f16757edc8e3526dad5
Suresh Karthick MuthuSelvam <sureshkarthick37@gmail.com>
2020-06-25 08:49:02 +0000

    test obj added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  columnDefs = [
-		{headerName: 'Make', field: 'make' },
-		{headerName: 'Model', field: 'model' },
-		{headerName: 'Price', field: 'price', editable: true}
-	];
+    columnDefs = [
+        { headerName: 'Brand Name', field: 'brandName' },
+        { headerName: 'Price', field: 'price' },
+        { headerName: 'Model', field: 'model' }, // , editable: true
+        { headerName: 'Quantity', field: 'quantity' }
+    ];
 
-	rowData = [
-		{ make: 'Toyota', model: 'Celica', price: 35000 },
-		{ make: 'Ford', model: 'Mondeo', price: 32000 },
-		{ make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
+    rowData = [
+        { brandName: 'Redmi', price: 13999, model: 'Note 9 Pro', quantity: 10  },
+        { brandName: 'One Plus', price: 42999 , model: 'OP 8', quantity: 30  },
+        { brandName: 'Realme', price: 15999, model: 'Realme 6', quantity: 7  },
+        { brandName: 'Samsung', price: 14999, model: 'M30s', quantity: 20  },
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
