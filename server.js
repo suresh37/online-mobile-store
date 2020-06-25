9d0ad6f59198ec462af69c396a3111ae0f970373
Suresh Karthick MuthuSelvam <sureshkarthick37@gmail.com>
2020-06-25 09:48:47 +0000

    mime type issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/online-mobile-store'));
 
 /* app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/online-mobile-store/'}
