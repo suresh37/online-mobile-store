10c60aac289c1e4194fd1bd466cbcd9722b367e7
Suresh Karthick MuthuSelvam <sureshkarthick37@gmail.com>
2020-06-25 09:20:50 +0000

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+
+var express = require('express')
+var app = express()
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+
+/* app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/online-mobile-store/'}
+);
+}); */
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
