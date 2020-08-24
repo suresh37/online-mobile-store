1ea62eaabc154ea181809f7b335ac6f38c984c96
Suresh Karthick MuthuSelvam <sureshkarthick37@gmail.com>
2020-08-24 11:06:16 +0000

    navbar updated

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { NavbarComponent } from './navbar.component';
 import { NavbarRoutingModule } from './navbar-routing.module';
+import { SharedModule } from './../shared/shared.module';
 //import { ProductComponent } from './../product/product.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        SharedModule,
         NavbarRoutingModule
             ],
     declarations: [
